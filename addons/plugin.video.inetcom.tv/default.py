58eb0b0aee36208656730a2ed7de73d83624339a
otaranda <otaranda@c2b5ea2d-a13b-7849-24bc-a52b5e5011fe>
2012-11-13 15:24:44 +0000

    Archives for inetcom.tv

--- addons/plugin.video.inetcom.tv/default.py
+++ addons/plugin.video.inetcom.tv/default.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python
 # Writer (c) 2012, Silhouette, E-mail: otaranda@hotmail.com
-# Rev. 0.1.4
+# Rev. 0.2.0
 
 
 
 import urllib,urllib2,re,sys,os,time
 import xbmcplugin,xbmcgui,xbmcaddon
+import json
 
 pluginhandle = int(sys.argv[1])
 __addon__       = xbmcaddon.Addon(id='plugin.video.inetcom.tv') 
@@ -15,19 +16,28 @@ __addon__       = xbmcaddon.Addon(id='plugin.video.inetcom.tv')
 
 INC_url = 'http://inetcom.tv'
 INC_ch = '/channel/all'
+INC_pr = '/tvprogram/index'
+INC_cat = '/tvprogram/ajaxShowChannels/cat/'
+INC_date = '/date/'
+INC_ajch = '/tvprogram/ajaxShowChannel/ch/'
 
 
 dbg = 0
 def dbg_log(line):
   if dbg: xbmc.log(line)
 
-def get_url(url, data = None, cookie = None, save_cookie = False, referrer = None):
+def get_url(url, data = None, cookie = None, save_cookie = False, referrer = None, accept = None):
     req = urllib2.Request(url)
-    req.add_header('User-Agent', 'Opera/9.80 (X11; Linux i686; U; ru) Presto/2.7.62 Version/11.00')
-    req.add_header('Accept', 'text/html, application/xml, application/xhtml+xml, */*')
-    req.add_header('Accept-Language', 'ru,en;q=0.9')
-    if cookie: req.add_header('Cookie', cookie)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 5.1; rv:16.0) Gecko/20100101 Firefox/16.0')
+    if accept == None: req.add_header('Accept', 'text/html, application/xml, application/xhtml+xml, */*')
+    if accept == 'json': req.add_header('Accept', 'application/json, text/javascript, */*; q=0.01')
+    req.add_header('Accept-Language', 'en-US,en;q=0.5')
+    req.add_header('Accept-Encoding', 'gzip, deflate')
+    req.add_header('Connection:', 'keep-alive')
+    req.add_header('X-Requested-With', 'XMLHttpRequest')
     if referrer: req.add_header('Referer', referrer)
+    if cookie: req.add_header('Cookie', cookie)
+
     if data: 
         response = urllib2.urlopen(req, data)
     else:
@@ -42,9 +52,29 @@ def get_url(url, data = None, cookie = None, save_cookie = False, referrer = Non
     response.close()
     return link
 
+
+def INC_start():
+    
+    dbg_log('-INC_start')
+
+    if 1:
+        name='Live TV'
+        item = xbmcgui.ListItem(name)
+        uri = sys.argv[0] + '?mode=live'
+        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+    
+        name='Archives'
+        item = xbmcgui.ListItem(name)
+        uri = sys.argv[0] + '?mode=chls'
+        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
+    
+        xbmcplugin.endOfDirectory(pluginhandle)  
+    else:
+        DTV_online(DTV_url, 'prls')
         
-def INC_prls(url):
-    dbg_log('INC_prls')
+                
+def INC_live(url):
+    dbg_log('INC_live')
     
     http = get_url(url, save_cookie = True, referrer = INC_url)
     mycookie = re.search('<cookie>(.+?)</cookie>', http).group(1)
@@ -54,11 +84,10 @@ def INC_prls(url):
 
     if len(pr_ls):
         for href,logo,descr in pr_ls:
-
             name = descr
             dbg_log(name)
             item = xbmcgui.ListItem(name, iconImage=INC_url + logo, thumbnailImage=INC_url + logo)
-            uri = sys.argv[0] + '?mode=PLAY'
+            uri = sys.argv[0] + '?mode=play'
             uri += '&url='+urllib.quote_plus(INC_url + href) + '&cook=' + urllib.quote_plus(mycookie)
             item.setInfo( type='video', infoLabels={'title': name, 'plot': descr})
             item.setProperty('IsPlayable', 'true')
@@ -66,17 +95,96 @@ def INC_prls(url):
             xbmcplugin.addDirectoryItem(pluginhandle, uri, item)
 
     xbmcplugin.endOfDirectory(pluginhandle)    
-    
-def INC_play(url, name, thumbnail, plot, mycookie):
+
+
+def INC_play(url, mycookie):
     dbg_log('-INC_play')
-    response    = get_url(url, cookie = mycookie, referrer = INC_url + INC_ch)
-    lnks_ls   = re.compile("lnks = \['(.+?)'\];").findall(response)
+    response = get_url(url, cookie = mycookie, referrer = INC_url + INC_ch)
+    lnks_ls = re.compile("lnks = \['(.+?)'\];").findall(response)
 
     if len(lnks_ls):
         dbg_log(lnks_ls[0])
         item = xbmcgui.ListItem(path =  lnks_ls[0])
         xbmcplugin.setResolvedUrl(pluginhandle, True, item)
-             
+
+
+
+def INC_chls():
+    dbg_log('-INC_chls')
+    http = get_url(INC_url + INC_ch, save_cookie = True, referrer = INC_url)
+    mycookie = re.search('<cookie>(.+?)</cookie>', http).group(1)
+    dbg_log(mycookie)
+    oneline = re.sub('[\r\n]', ' ', http)
+    pr_ls = re.compile('<tr > +?<td class="col01"><a href="(.+?)"><img src="(.+?)"  alt="(.+?)" /></a></td>').findall(oneline)
+    print pr_ls
+    if len(pr_ls):
+        for href,logo,name in pr_ls:
+            dbg_log(name)
+            item = xbmcgui.ListItem(name, iconImage=INC_url + logo, thumbnailImage=INC_url + logo)
+            uri = sys.argv[0] + '?mode=dtls'
+            uri += '&url='+urllib.quote_plus(INC_url + href) + '&name='+urllib.quote_plus(name)
+            uri += '&thumbnail=' + urllib.quote_plus(INC_url + logo)
+            uri += '&cook=' + urllib.quote_plus(mycookie)
+            xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+            dbg_log(uri)
+
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+
+def INC_dtls(url, name, thumbnail, mycookie):
+    dbg_log('-INC_dtls')
+    response = get_url(url, cookie = mycookie, referrer = INC_url + INC_ch)
+    id_ls = re.compile("currentChId=(.+?);").findall(response)
+    if len(id_ls) == 0:
+        id_ls = re.compile("var current_channel = (.+?);").findall(response)
+    if len(id_ls):
+        dbg_log(id_ls[0])
+        dtls = re.compile('<li class="(.*?)"><a href="(.*?)">..</a><span class="date">').findall(response)
+        if len(dtls):
+            today = False
+            for cl, dt in reversed(dtls):
+                if cl == 'active': today = True
+                if today:
+                    item = xbmcgui.ListItem(dt, iconImage=thumbnail, thumbnailImage=thumbnail)
+                    uri = sys.argv[0] + '?mode=prls'
+                    uri += '&url='+urllib.quote_plus(INC_url + INC_ajch + id_ls[0] + INC_date + dt)
+                    uri += '&name='+urllib.quote_plus(name) + '&pdate='+urllib.quote_plus(dt)
+                    uri += '&id='+urllib.quote_plus(id_ls[0]) + '&thumbnail=' + urllib.quote_plus(thumbnail)
+                    uri += '&cook=' + urllib.quote_plus(mycookie)
+                    xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+    
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+
+def INC_prls(url, id, name, pdate, thumbnail, mycookie):
+    dbg_log('-INC_prls')
+    response = get_url(url, cookie = mycookie, referrer = INC_url + INC_ch)
+    jdata = json.loads(response, encoding="cp1251")
+    #print json.dumps(jdata, encoding="cp1251")
+    
+    for it in jdata:
+      if it == 'schedules':
+          for ent in jdata[it]:
+              item = xbmcgui.ListItem(ent[u'startTime'] + '-' + ent[u'title'], iconImage=thumbnail, thumbnailImage=thumbnail)
+              uri = sys.argv[0] + '?mode=arpl'
+              uri += '&id='+urllib.quote_plus(id) + '&name='+urllib.quote_plus(name)
+              uri += '&pdate='+urllib.quote_plus(pdate) + '&ptime='+urllib.quote_plus(ent[u'startTime'])
+              item.setInfo( type='video', infoLabels={'title': name, 'plot': name})
+              item.setProperty('IsPlayable', 'true')              
+              xbmcplugin.addDirectoryItem(pluginhandle, uri, item)
+
+    xbmcplugin.endOfDirectory(pluginhandle)
+
+
+def INC_arpl(url, id, name, pdate, ptime):
+    dbg_log('-INC_arpl')
+    vlink = 'http://194.187.205.237' + '/video/' + id + '/' + pdate + '/' + ptime + '_' + name +'.mkv'
+    dbg_log(vlink)
+    item = xbmcgui.ListItem(path =  vlink)
+    xbmcplugin.setResolvedUrl(pluginhandle, True, item)
+        
+        
+
    
 def get_params():
     param=[]
@@ -97,11 +205,13 @@ def get_params():
 
 params=get_params()
 url=None
+id=''
 name=''
+pdate=''
+ptime=''
 plot=''
 cook=''
 mode=None
-#thumbnail=fanart
 thumbnail=''
 
 dbg_log('OPEN:')
@@ -119,8 +229,20 @@ try:
     dbg_log('-COOK:'+ cook + '\n')
 except: pass
 try:
+    id=urllib.unquote_plus(params['id'])
+    dbg_log('-ID:'+ id + '\n')
+except: pass
+try:
     name=urllib.unquote_plus(params['name'])
     dbg_log('-NAME:'+ name + '\n')
+except: pass
+try:
+    pdate=urllib.unquote_plus(params['pdate'])
+    dbg_log('-PDATE:'+ pdate + '\n')
+except: pass
+try:
+    ptime=urllib.unquote_plus(params['ptime'])
+    dbg_log('-PTIME:'+ ptime + '\n')
 except: pass
 try: 
     thumbnail=urllib.unquote_plus(params['thumbnail'])
@@ -136,8 +258,13 @@ except: pass
 
 
 
-if mode == 'PLAY': INC_play(url, name, thumbnail, plot, cook)
-elif mode == None: INC_prls(INC_url + INC_ch)
+if mode == 'play': INC_play(url, cook)
+elif mode == 'live': INC_live(INC_url + INC_ch)
+elif mode == 'chls': INC_chls()
+elif mode == 'dtls': INC_dtls(url, name, thumbnail, cook)
+elif mode == 'prls': INC_prls(url, id, name, pdate, thumbnail, cook)
+elif mode == 'arpl': INC_arpl(url, id, name, pdate, ptime)
+elif mode == None: INC_start()
 
 dbg_log('CLOSE:')
 
