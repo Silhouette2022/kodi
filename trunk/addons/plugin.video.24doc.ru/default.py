77c2f202d2f0132618ac76d97c6457497ed8a689
Silhouette2022@gmail.com <Silhouette2022@gmail.com@c2b5ea2d-a13b-7849-24bc-a52b5e5011fe>
2013-06-04 20:41:32 +0000

    new rev of 24doc

--- trunk/addons/plugin.video.24doc.ru/default.py
+++ trunk/addons/plugin.video.24doc.ru/default.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Writer (c) 2013, Silhouette, E-mail:
-# Rev. 0.1.1
+# Rev. 0.2.0
 
 
 import urllib,urllib2,re,sys
@@ -31,12 +31,15 @@ def get_logo(key):
     return logo
             
 start_pg = "http://24doc.ru"
-proj_pg = start_pg + "/projects"
+proj_pg = start_pg + "/project"
+online_pg = start_pg + "/movies"
+page_pg = "?page="
 
 def dbg_log(line):
     if dbg: print line
 
 def get_url(url, data = None, cookie = None, save_cookie = False, referrer = None):
+    dbg_log('-getURL:'+  url + '\n')
     req = urllib2.Request(url)
     req.add_header('User-Agent', 'Opera/9.80 (X11; Linux i686; U; ru) Presto/2.7.62 Version/11.00')
     req.add_header('Accept', 'text/html, application/xml, application/xhtml+xml, */*')
@@ -60,101 +63,147 @@ def get_url(url, data = None, cookie = None, save_cookie = False, referrer = Non
 # *******************************************************************  
 # *******************************************************************  
 # *******************************************************************  
-  
-def D24_start(url):
-    dbg_log('-D24_start:' + '\n')
+def D24_start():
+    
+    dbg_log('-D24_start')
+
+    name='Фильмы-Онлайн'
+    item = xbmcgui.ListItem(name)
+    uri = sys.argv[0] + '?mode=films&url=' + urllib.quote_plus(online_pg)
+    xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+
+    name='Анонсы/Preview'
+    item = xbmcgui.ListItem(name)
+    uri = sys.argv[0] + '?mode=pall&url=' + urllib.quote_plus(proj_pg)
+    xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
+
+    xbmcplugin.endOfDirectory(pluginhandle)  
+
+   
+def D24_pall(url, page):
+    dbg_log('-ND24_proj:' + '\n')
     dbg_log('- url:'+  url + '\n')
 
-    http = get_url(url)
-    nav = BeautifulSoup(http).findAll('ul',{"class":"content-nav"})
+    http = get_url(url + page_pg + page)
+    prj = BeautifulSoup(http).findAll('ul',{"class":"projects"})
 
-    if len(nav):
-        prj_ls = re.compile('<a class="link" href="(.*?)"><span class="bg"><span>(.*?)</span>(.*?)</span></a>').findall(str(nav[0]))
-        for href, title1, title2 in prj_ls:
-            img = get_logo(title1)
-            if title2 != '': title1 += '-'+title2
-            item = xbmcgui.ListItem(title1, iconImage=img, thumbnailImage=img)
-            uri = sys.argv[0] + '?mode=proj' + '&url=' + urllib.quote_plus(href)\
-            + '&img=' + urllib.quote_plus(img)
+    i = 0
+    if len(prj):
+        link_ls = re.compile('<a class="link" href="(.*?)">(.*?)</a>').findall(str(prj[0]))
+        img = xbmc.translatePath( __addon__.getAddonInfo('path') + '\\icon.png')
+        for href, title in link_ls:
+            item = xbmcgui.ListItem(title, iconImage=img, thumbnailImage=img)
+            uri = sys.argv[0] + '?mode=pview' + '&url=' + urllib.quote_plus(href)\
+             + '&name=' + urllib.quote_plus(title)
             xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
             dbg_log('- uri:'+  uri + '\n')
+            i += 1
+            
+    if i:
+        item = xbmcgui.ListItem('<NEXT PAGE>')
+        uri = sys.argv[0] + '?mode=pall&page=' + str(int(page) + 1) + '&url=' + urllib.quote_plus(url)
+        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+        dbg_log('- uri:'+  uri + '\n')
         
     xbmcplugin.endOfDirectory(pluginhandle)
     
 
-def D24_proj(url, img):
-    dbg_log('-ND24_proj:' + '\n')
-    dbg_log('- url:'+  url + '\n')
-
-    http = get_url(start_pg + url)
-    prj = BeautifulSoup(http).findAll('ul',{"class":"projects"})
-
-    if len(prj):
-        link_ls = re.compile('<a class="link" href="(.*?)">(.*?)</a>').findall(str(prj[0]))
-        for href, title in link_ls:
-            item = xbmcgui.ListItem(title, iconImage=img, thumbnailImage=img)
-            uri = sys.argv[0] + '?mode=view' + '&url=' + urllib.quote_plus(href)\
-             + '&name=' + urllib.quote_plus(title)
-            xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
-            dbg_log('- uri:'+  uri + '\n')
-        
-    xbmcplugin.endOfDirectory(pluginhandle)
-
-def D24_view(url, name):     
+def D24_pview(url, name):     
     dbg_log('-D24_view:'+ '\n')
     dbg_log('- url:'+  url + '\n')
         
     xbmcplugin.setContent(int(sys.argv[1]), 'episodes')#movies episodes tvshows
         
     http = get_url(start_pg + url)
-    cont = BeautifulSoup(http).findAll('div',{"class":"content-holder"})
+    oneline = re.sub('[\n\r\t]',' ',http)
+    img_ls = re.compile('<div class="preview_play"></div> *?<a href=".*?"> *?<img src="(.*?)" alt').findall(oneline)
+    lnk_ls = re.compile('<div class="tv_show_desc"> *?<a href="(.*?)">(.*?)</a>').findall(oneline)
+    plot_ls = re.compile('<div class="about_video_toggle"> *?(.*?) *?</div>').findall(oneline)
 
-    if len(cont):
-        img_ls = re.compile('<img alt=".*?" height=".*?" src="(.*?)" width=".*?" />').findall(str(cont[0]))
-        if len(img_ls): img = re.sub(' ','%20', start_pg + img_ls[0])
-        else: img = '' 
-        print img
-        p_ls = re.compile('<p>(.*?)</p>').findall(re.sub('[\n\r\t]',' ',str(cont[0])))
-        plot = ''
-        for pi in p_ls: plot += pi
-        plot = re.sub('<br />','\n', plot)
+    if lnk_ls == []:
+        lnk_ls = [('', '[COLOR FFE13723]НЕТ ВИДЕО [/COLOR]')]
+        img_ls = re.compile('"background-image:url\(\'(.*?)\'\)').findall(oneline)
         
-        http = get_url(start_pg + url + '/videos')
-        prl = BeautifulSoup(http).findAll('ul',{"class":"project-list"})
+    i = 0
+    for href,dscr in lnk_ls:
+        if len(img_ls) > i: img = re.sub(' ','%20', start_pg + img_ls[i])
+        else: img = '' 
 
-        if len(prl):
-#        <div class="img-holder"><img alt="" height="138" src="/uploads/video/.thumbs/dd90084412067907f8aad3d19115ed05_d6a76b.jpg" width="184" /></div>
-#        <h2><a class="link" href="/project/4544/video/1806">Трилогия 9.11 (18+)</a></h2>
-            link_ls = re.compile('<a class="link" href="(.*?)">(.*?)</a>').findall(str(prl[0]))
-            for href,descr in link_ls:
-                http = get_url(start_pg + href)
-                
-                #<div class="dm_flow_player {&quot;clip&quot;:{&quot;url&quot;:&quot;\/uploads\/video\/60a96e9d8aacd97e3f224e59a84fa9e4.flv&quot;,&quot;autoPlay&quot;:true,&quot;scaling&quot;:&quot;fit&quot;},&quot;plugins&quot;:{&quot;controls&quot;:true},&quot;player_web_path&quot;:&quot;\/dmFlowPlayerPlugin\/swf\/lib\/flowplayer-3.1.5.swf&quot;,&quot;mimeGroup&quot;:&quot;video&quot;}" style="width: 592px; height: 378px; display: block;"></div></div>
-                #"url":"\/uploads\/video\/60a96e9d8aacd97e3f224e59a84fa9e4.flv"
-                flow_ls = re.compile('<div class="dm_flow_player(.*?)"></div>').findall(http)
-                if len(flow_ls):
-                    flv = re.sub('\\\/', '/', re.sub('&quot;','\"', flow_ls[0]))
-                    url_ls = re.compile('"url":"(.*?)"').findall(flv)
-                    if len(url_ls):
-                        item = xbmcgui.ListItem(name + '-' + descr, iconImage=img, thumbnailImage=img)
-                        uri = sys.argv[0] + '?mode=play' \
-                        + '&url=' + urllib.quote_plus(start_pg + url_ls[0])
-                        
-                        item.setInfo( type='video', infoLabels={'title': name, 'plot': plot})
-                        item.setProperty('IsPlayable', 'true')
-                        xbmcplugin.addDirectoryItem(pluginhandle, uri, item)  
-                        dbg_log('- uri:'+  uri + '\n')
+        title = dscr.strip() + '-' + name
+        if len(plot_ls):
+            plot = plot_ls[0].replace('<p>', '').replace('</p>', '').replace('<br />', '')
+        else:
+            plot = title
+        
+        item = xbmcgui.ListItem(title, iconImage=img, thumbnailImage=img)
+        uri = sys.argv[0] + '?mode=pplay' \
+        + '&url=' + urllib.quote_plus(start_pg + href)
+        
+        item.setInfo( type='video', infoLabels={'title': title, 'plot': plot})
+        item.setProperty('IsPlayable', 'true')
+        xbmcplugin.addDirectoryItem(pluginhandle, uri, item)  
+        dbg_log('- uri:'+  uri + '\n')
+        i += 1
+    xbmcplugin.endOfDirectory(pluginhandle)
+    
 
+def D24_pplay(url):     
+    dbg_log('-D24_pplay:'+ '\n')
+    dbg_log('- url:'+  url + '\n')
+
+    http = get_url(url)
+    src_ls = re.compile('<source src="(.*?)"').findall(http)
+    if len(src_ls):
+        item = xbmcgui.ListItem(path = start_pg + src_ls[0])
+        xbmcplugin.setResolvedUrl(pluginhandle, True, item)
+
+
+def D24_fplay(url):     
+    dbg_log('-D24_fplay:'+ '\n')
+    dbg_log('- url:'+  url + '\n')
+
+    item = xbmcgui.ListItem(path = url)
+    xbmcplugin.setResolvedUrl(pluginhandle, True, item)
+        
+
+def D24_films(url, page):
+    dbg_log('-ND24_films:' + '\n')
+    dbg_log('- url:'+  url + '\n')
+
+    xbmcplugin.setContent(int(sys.argv[1]), 'episodes')#movies episodes tvshows
+    
+    http = get_url(url + page_pg + page)
+    prj = BeautifulSoup(http).findAll('div',{"class":"video_block"})
+
+    i = 0
+    for i in xrange(len(prj)):
+        oneline = re.sub('[\n\r\t]',' ',str(prj[i]))
+        link_ls = re.compile('a href="(.*?)" title="(.*?)".*?data-description="(.*?)" data-id=".*?" data-video="(.*?)"').findall(oneline)
+        img_ls = re.compile('<img width=".*?" height=".*?" src="(.*?)" />').findall(oneline)
+        for href, title, plot, video in link_ls:
+            if len(img_ls): img = start_pg + img_ls[0]
+            else:  img = xbmc.translatePath( __addon__.getAddonInfo('path') + '\\icon.png')
+            
+            item = xbmcgui.ListItem(title, iconImage=img, thumbnailImage=img)
+            uri = sys.argv[0] + '?mode=fplay' \
+            + '&url=' + urllib.quote_plus(start_pg + video)
+            
+            item.setInfo( type='video', infoLabels={'title': title, 'plot': plot})
+            item.setProperty('IsPlayable', 'true')
+            xbmcplugin.addDirectoryItem(pluginhandle, uri, item)  
+            dbg_log('- uri:'+  uri + '\n')
+
+            
+    if i:
+        item = xbmcgui.ListItem('<NEXT PAGE>')
+        uri = sys.argv[0] + '?mode=films&page=' + str(int(page) + 1) + '&url=' + urllib.quote_plus(url)
+        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+        dbg_log('- uri:'+  uri + '\n')
+        
     xbmcplugin.endOfDirectory(pluginhandle)
 
 
-def D24_play(url):     
-    dbg_log('-D24_play:'+ '\n')
-    dbg_log('- url:'+  url + '\n')
     
-    item = xbmcgui.ListItem(path = url)
-    xbmcplugin.setResolvedUrl(pluginhandle, True, item)
-
 def get_params():
     param=[]
     #print sys.argv[2]
@@ -211,13 +260,14 @@ try:
     dbg_log('-COOK:'+ cook + '\n')
 except: pass
 
-if url=='':
-    url = proj_pg
+#if url=='':
+#    url = proj_pg
 
-if mode == '': D24_start(url)
-elif mode == 'proj': D24_proj(url, imag)
-elif mode == 'view': D24_view(url, name)
-elif mode == 'play': D24_play(url)
-
+if mode == '': D24_start()
+elif mode == 'pall': D24_pall(url, page)
+elif mode == 'pview': D24_pview(url, name)
+elif mode == 'pplay': D24_pplay(url)
+elif mode == 'films': D24_films(url, page)
+elif mode == 'fplay': D24_fplay(url)
 
 
