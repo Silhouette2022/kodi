09c83a94a0ef1afab4b9fb5af802407aa8658830
Silhouette2022@gmail.com <Silhouette2022@gmail.com@c2b5ea2d-a13b-7849-24bc-a52b5e5011fe>
2013-06-06 18:27:36 +0000

    added search and catalog to kinoxa-x

--- addons/plugin.video.kinoxa-x.ru/default.py
+++ addons/plugin.video.kinoxa-x.ru/default.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 # Writer (c) 2013, Silhouette, E-mail: 
-# Rev. 0.1.0
+# Rev. 0.2.0
 
 
 import urllib,urllib2,re,sys
 import xbmcplugin,xbmcgui,xbmcaddon
 from BeautifulSoup import BeautifulSoup
 
-dbg = 0
+dbg = 1
 
 pluginhandle = int(sys.argv[1])
 
 start_pg = "http://www.kinoxa-x.ru"
 page_pg = start_pg + "/load/0-"
+fdpg_pg = ";t=0;md=;p="
 find_pg = start_pg + "/search/?q="
 
 
@@ -41,45 +42,51 @@ def get_url(url, data = None, cookie = None, save_cookie = False, referrer = Non
     response.close()
     return link
 
-def KNX_list(url, page, cook):
+def KNX_list(url, page, type):
     dbg_log('-KNX_list:' + '\n')
     dbg_log('- url:'+  url + '\n')
     dbg_log('- page:'+  page + '\n')
-    dbg_log('- cook:'+  cook + '\n')    
-    ext_ls = [('<КАТАЛОГ>', '?mode=ctlg'),
-              ('<ПОИСК>', '?mode=find')]
-#    if url.find(find_pg) != -1:
-#        n_url = url + search_start + page
-#    else:
-    n_url = page_pg + page
+    
+    if type == 'ctlg':
+        n_url = url + '-' + page + '-2'
+    else:
+        n_url = url + page
         
     dbg_log('- n_url:'+  n_url + '\n')
     
     xbmcplugin.setContent(int(sys.argv[1]), 'episodes')#movies episodes tvshows
     
-    http = get_url(n_url, cookie = cook, save_cookie = True)
-#    if cook=='':
-#        cook = re.search('<cookie>(.+?)</cookie>', http).group(1)
+    http = get_url(n_url)
     i = 0
     
-    for ctTitle, ctMode  in ext_ls:
-        item = xbmcgui.ListItem(ctTitle)
-        uri = sys.argv[0] + ctMode + '&cook=' + urllib.quote_plus(cook)
-        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
-        dbg_log('- uri:'+  uri + '\n')    
+    if type == '':
+        ext_ls = [('<КАТАЛОГ>', '?mode=ctlg'),
+                  ('<ПОИСК>', '?mode=find')]
+        for ctTitle, ctMode  in ext_ls:
+            item = xbmcgui.ListItem(ctTitle)
+            uri = sys.argv[0] + ctMode
+            xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
+            dbg_log('- uri:'+  uri + '\n')    
     
-
-    entry_id = re.compile("entryID[0-9]")
-    entrys = BeautifulSoup(http).findAll('div',{"id":entry_id})
-
+    if type == 'find':
+        entrys = BeautifulSoup(http).findAll('div',{"class":"eTitle"})
+        msgs = BeautifulSoup(http).findAll('div',{"class":"eMessage"})
+    else:
+        entry_id = re.compile("entryID[0-9]")
+        entrys = BeautifulSoup(http).findAll('div',{"id":entry_id})
+ 
     for eid in entrys:
-
-
-        films = re.compile('<a href="(.*?)"><h3 style=".*?">(.*?)</h3></a>').findall(str(eid))
-        plots = re.compile('<span class="kr-opis-rol">(.*?)</noindex>').findall(re.sub('[\n\r\t]', ' ',str(eid)))
-        imgs = re.compile('<img src="(.*?)"').findall(str(eid))
+        
+        if type == 'find':
+            films = re.compile('<a href="(.*?)">(.*?)</a>').findall(str(eid))
+            plots = re.compile('style=".*?">(.*?)</div>').findall(re.sub('[\n\r\t]', ' ',str(msgs[i])))
+        else:
+            films = re.compile('<a href="(.*?)"><h3 style=".*?">(.*?)</h3></a>').findall(str(eid))
+            plots = re.compile('<span class="kr-opis-rol">(.*?)</noindex>').findall(re.sub('[\n\r\t]', ' ',str(eid)))
+            imgs = re.compile('<img src="(.*?)"').findall(str(eid))
         
         for href, title in films:
+            title = re.sub('<.*?>', '',title)
             try: img = imgs[0]
             except: img = ''
             try: plot = re.sub('<.*?>', '',plots[0])
@@ -99,53 +106,67 @@ def KNX_list(url, page, cook):
                 
     if i:
         item = xbmcgui.ListItem('<NEXT PAGE>')
-        uri = sys.argv[0] + '?page=' + str(int(page) + 1) + '&url=' + urllib.quote_plus(url)+ '&cook=' + urllib.quote_plus(cook)
+        uri = sys.argv[0] + '?page=' + str(int(page) + 1) + '&url=' + urllib.quote_plus(url) + '&type=' + type
         xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
         dbg_log('- uri:'+  uri + '\n')
-        item = xbmcgui.ListItem('<NEXT PAGE +5>')
-        uri = sys.argv[0] + '?page=' + str(int(page) + 5) + '&url=' + urllib.quote_plus(url)+ '&cook=' + urllib.quote_plus(cook)
-        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
-        dbg_log('- uri:'+  uri + '\n')        
+        if type != 'find':
+            item = xbmcgui.ListItem('<NEXT PAGE +5>')
+            uri = sys.argv[0] + '?page=' + str(int(page) + 5) + '&url=' + urllib.quote_plus(url) + '&type=' + type
+            xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+            dbg_log('- uri:'+  uri + '\n')        
  
     xbmcplugin.endOfDirectory(pluginhandle) 
 
 
-def KNX_view(url, img, name, cook):     
-    dbg_log('-NKN_view:'+ '\n')
+def KNX_list2(url, page):
+    dbg_log('-KNX_list:' + '\n')
     dbg_log('- url:'+  url + '\n')
-    dbg_log('- img:'+  img + '\n')
-    dbg_log('- name:'+  name + '\n')
+    dbg_log('- page:'+  page + '\n')
+
+
+    n_url = url + page
         
-    http = get_url(url, cookie = cook)
-    news_id = re.compile("news-id-[0-9]")
-    news = BeautifulSoup(http).findAll('div',{"id":news_id})
+    dbg_log('- n_url:'+  n_url + '\n')
+    
+    xbmcplugin.setContent(int(sys.argv[1]), 'episodes')#movies episodes tvshows
+    
+    http = get_url(n_url)
+    i = 0
+    
+    entrys = BeautifulSoup(http).findAll('div',{"class":"eTitle"})
+    msgs = BeautifulSoup(http).findAll('div',{"class":"eMessage"})
 
-    for sa in news:    
-        #print str(sa)
-        flvars = re.compile('<param name="flashvars" value="(.*?)"').findall(str(sa))
-        #print urllib.unquote_plus(flvars[0])
-        files = re.compile('file=(.*?)"').findall(str(sa))
+    for eid in entrys:
 
-        i = 1
-        for file in files:
-            if len(files) > 1:
-                title = str(i) + ' - ' + name
-            else:
-                title = name
-            i += 1
+        films = re.compile('<a href="(.*?)">(.*?)</a>').findall(str(eid))
+        plots = re.compile('style=".*?">(.*?)</div>').findall(re.sub('[\n\r\t]', ' ',str(msgs[i])))
+        
+        for href, title in films:
+            img = ''
+            title = re.sub('<.*?>', '',title)
+            try: plot = re.sub('<.*?>', '',plots[0])
+            except: plot = title
+            dbg_log('-TITLE %s'%title)
+            dbg_log('-PLOT %s'%plot)
 
             item = xbmcgui.ListItem(title, iconImage=img, thumbnailImage=img)
+            item.setInfo( type='video', infoLabels={'title': title, 'plot': plot})
             uri = sys.argv[0] + '?mode=play' \
-            + '&url=' + urllib.quote_plus(file) + '&cook=' + urllib.quote_plus(cook)
+            + '&url=' + urllib.quote_plus(href)
             item.setProperty('IsPlayable', 'true')
-            xbmcplugin.addDirectoryItem(pluginhandle, uri, item)  
+            xbmcplugin.addDirectoryItem(pluginhandle, uri, item, False)  
             dbg_log('- uri:'+  uri + '\n')
-
-        xbmcplugin.endOfDirectory(pluginhandle)
-
-
-
-
+            i = i + 1
+                
+    if i:
+        item = xbmcgui.ListItem('<NEXT PAGE>')
+        uri = sys.argv[0] + '?mode=list&page=' + str(int(page) + 1) + '&url=' + urllib.quote_plus(url)
+        xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)
+        dbg_log('- uri:'+  uri + '\n')
+      
+ 
+    xbmcplugin.endOfDirectory(pluginhandle) 
+    
 def KNX_play(url):     
     dbg_log('-NKN_play:'+ '\n')
     dbg_log('- url:'+  url + '\n')
@@ -160,61 +181,77 @@ def KNX_play(url):
             item = xbmcgui.ListItem(path = flvs[0])
             xbmcplugin.setResolvedUrl(pluginhandle, True, item)
 
-def NKN_ctlg(url, cook):
-    dbg_log('-NKN_ctlg:' + '\n')
+def KNX_ctlg(url):
+    dbg_log('-KNX_ctlg:' + '\n')
     dbg_log('- url:'+  url + '\n')
-
-    catalog = [("komedii/", "Комедии"),
-               ("boeviki/", "Боевики"),
-               ("trillery/", "Триллеры"),
-               ("detektivnye/", "Детективные"),
-               ("voennye/", "Военные"),
-               ("otechestvennye/", "Отечественные"),
-               ("istoricheskie/", "Исторические"),
-               ("semejjnye/", "Семейные"),
-               ("prikljuchencheskie/", "Приключенческие"),
-               ("animacionnye/", "Анимационные"),
-               ("dokumentalnye/", "Документальные"),
-               ("serialy/", "Сериалы"),
-               ("fantasticheskie/", "Фантастические"),
-               ("misticheskie/", "Мистические"),
-               ("uzhasy/", "Ужасы"),
-               ("fjentezi/", "Фэнтези"),
-               ("dramy/", "Драмы"),
-               ("melodramy/", "Мелодрамы"),
-               ("kriminalnye/", "Криминальные"),
-               ("jumor/", "Юмор"),
-               ("oskar/", "Премия Оскар")]
+               
+    catalog  = [( "/load/sortirovka_po_godam/filmy_2013_goda/68", '2013 год'),
+                ( "/load/sortirovka_po_godam/filmy_2012_goda/65", '2012 год'),
+                ( "/load/sortirovka_po_godam/filmy_2011_goda/51", '2011 год'),
+                ( "/load/sortirovka_po_godam/filmy_2010_goda/52", '2010 год'),
+                ( "/load/sortirovka_po_godam/filmy_2009_goda/53", '2009 год'),
+                ( "/load/kategorii/filmy_v_3d/67", 'Фильмы в 3D'),
+                ( "/load/kategorii/komedii/30", 'Комедии'),
+                ( "/load/kategorii/boeviki/31", 'Боевики'),
+                ( "/load/kategorii/dramy/32", 'Драмы'),
+                ( "/load/kategorii/uzhasy/33", 'Ужасы'),
+                ( "/load/kategorii/fantastika/34", 'Фантастика'),
+                ( "/load/kategorii/trillery/35", 'Триллеры'),
+                ( "/load/kategorii/detektivy/36", 'Детективы'),
+                ( "/load/kategorii/dokumentalnye/37", 'Документальные'),
+                ( "/load/kategorii/multfilmy/38", 'Мультфильмы'),
+                ( "/load/kategorii/mistika/39", 'Мистика'),
+                ( "/load/kategorii/sovetskie_filmy/69", 'Советские фильмы'),
+                ( "/load/kategorii/semejnye/40", 'Семейные'),
+                ( "/load/kategorii/sportivnye/41", 'Спортивные'),
+                ( "/load/kategorii/melodramy/42", 'Мелодрамы'),
+                ( "/load/kategorii/russkie_serialy/43", 'Русские сериалы'),
+                ( "/load/kategorii/zarubezhnye_serialy/64", 'Зарубежные сериалы'),
+                ( "/load/kategorii/trejlery/44", 'Трейлеры'),
+                ( "/load/kategorii/fehntezi/46", 'Фэнтези'),
+                ( "/load/kategorii/prikljuchenija/47", 'Приключения'),
+                ( "/load/kategorii/istoricheskie/48", 'Исторические'),
+                ( "/load/kategorii/vestern/49", 'Вестерн'),
+                ( "/load/kategorii/animeh/56", 'Аниме'),
+                ( "/load/kategorii/kriminal/60", 'Криминал'),
+                ( "/load/kategorii/voennye/61", 'Военные'),
+                ( "/load/kategorii/goblinskij_perevod/55", 'Гоблинский перевод'),
+                ( "/load/kategorii/poznaem_mir/57", 'Познаем мир'),
+                ( "/load/kategorii/biografija/58", 'Биография'),
+                ( "/load/kategorii/xxx/59", 'XXX'),
+                ( "/load/kategorii/jumoristicheskie_peredachi/62", 'Юмористические передачи'),
+                ( "/load/kategorii/teleshou/63", 'Передачи'),
+                ( "/load/kategorii/filmy_v_vysokom_kachestve_hd/54", 'В высоком качестве HD')]
+               
                
     for ctLink, ctTitle  in catalog:
         item = xbmcgui.ListItem(ctTitle)
         uri = sys.argv[0] \
-        + '?url=' + urllib.quote_plus(start_pg + ctLink) + '&cook=' + urllib.quote_plus(cook)
+        + '?url=' + urllib.quote_plus(start_pg + ctLink) + '&type=ctlg&page=1'
         xbmcplugin.addDirectoryItem(pluginhandle, uri, item, True)  
         dbg_log('- uri:'+  uri + '\n')
         
     xbmcplugin.endOfDirectory(pluginhandle)
 
-def uni2cp(ustr):
+def uni2enc(ustr):
     raw = ''
     uni = unicode(ustr, 'utf8')
     uni_sz = len(uni)
-    for i in range(uni_sz):
-        raw += ('%%%02X') % ord(uni[i].encode('cp1251'))
-    return raw  
-
-def NKN_find(cook):     
-    dbg_log('-NKN_find:'+ '\n')
-    dbg_log('- cook:'+  cook + '\n')      
+    for i in xrange(len(ustr)):
+        raw += ('%%%02X') % ord(ustr[i])        
+    return raw
+    
+def KNX_find():     
+    dbg_log('-KNX_find:'+ '\n')
     
     kbd = xbmc.Keyboard()
     kbd.setHeading('ПОИСК')
     kbd.doModal()
     if kbd.isConfirmed():
-        stxt = uni2cp(kbd.getText())
-        furl = find_pg + stxt
+        stxt = uni2enc(kbd.getText())
+        furl = find_pg + stxt + fdpg_pg
         dbg_log('- furl:'+  furl + '\n')
-        NKN_start(furl, '1', cook)
+        KNX_list(furl, '1', 'find')
 
 def lsChan():
     xbmcplugin.endOfDirectory(pluginhandle)
@@ -240,15 +277,9 @@ def get_params():
 params=get_params()
 
 
-cook = ''
+type = ''
 mode=''
 url=''
-ordr='0'
-dir='0'
-off='0'
-gnrs=''
-imag=''
-name=''
 
 try:
     mode=params['mode']
@@ -263,26 +294,17 @@ try:
     dbg_log('-PAGE:'+ page + '\n')
 except: page = '1'
 try: 
-    imag=urllib.unquote_plus(params['img'])
-    dbg_log('-IMaG:'+ imag + '\n')
-except: pass 
-try: 
-    name=urllib.unquote_plus(params['name'])
-    dbg_log('-NAME:'+ name + '\n')
-except: pass 
-try: 
-    cook=urllib.unquote_plus(params['cook'])
-    dbg_log('-COOK:'+ cook + '\n')
+    type=urllib.unquote_plus(params['type'])
+    dbg_log('-TYPE:'+ type + '\n')
 except: pass
 
 if url=='':
-    url = start_pg
+    url = page_pg
 
-if mode == '': KNX_list(url, page, cook)
-elif mode == 'ctlg': NKN_ctlg(url, cook)
-elif mode == 'view': NKN_view(url, imag, name, cook)
+if mode == '': KNX_list(url, page, type)
+elif mode == 'ctlg': KNX_ctlg(url)
 elif mode == 'play': KNX_play(url)
-elif mode == 'find': NKN_find(cook)
-
+elif mode == 'find': KNX_find()
+#elif mode == 'list': KNX_list(url, page)
 
 
