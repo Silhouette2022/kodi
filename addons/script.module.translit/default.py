b879655b6f43511c4c3b37c591c57f937a14303e
silhouette2022 <silhouette2022@c2b5ea2d-a13b-7849-24bc-a52b5e5011fe>
2015-01-10 05:19:05 +0000

    deleted other repo files

--- addons/script.module.translit/default.py
+++
@@ -1,9 +0,0 @@
-import sys
-import xbmc
-import xbmcgui
-import xbmcplugin
-import xbmcaddon
-
-settings = xbmcaddon.Addon(id='script.module.translit')
-version = "1.0.3"
-plugin = "Translit-" + version
